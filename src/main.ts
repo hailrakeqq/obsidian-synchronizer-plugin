ed95351ebd15827bd7dd474c88a307638debf4c2
Yurii Boklan <hailrake@riseup.net>
2026-05-27 21:43:33 +0300

    release 0.1

--- src/main.ts
+++ src/main.ts
@@ -9,50 +9,74 @@ import { DownloadModal } from "./download_modal";
 export default class ObsidianSynchronizerPlugin extends Plugin {
 	settings!: ObsidianSynchronizerPluginSettings;
 	
+	async upload(){
+		new Notice('Upload to server!' + ' Server IP: ' + this.settings.serverIP);
+		const adapter = this.app.vault.adapter;
+		
+		if (adapter instanceof FileSystemAdapter) {
+			const vaultPath = adapter.getBasePath();
+			const vaultPathArray = vaultPath.split('/')
+			const vaultName = vaultPathArray[vaultPathArray.length - 1]
+			const tmpDir = path.join(os.tmpdir(), "obsidian-synchronizer");
+			const zipPath = path.join(tmpDir, "vault-latest.zip");
+			if (!fs.existsSync(tmpDir)) 
+    			fs.mkdirSync(tmpDir, { recursive: true });
+			
+			await Toolchain.createZipArchive(vaultPath, zipPath)
+			const uploadResult = await Toolchain.uploadToServer(this.settings.serverIP, zipPath, vaultName as any, this.settings.API_TOKEN)
+
+			if(uploadResult == 200) {
+				fs.rmdirSync(tmpDir, {recursive: true})
+				new Notice('Archive have been successfully uploaded to server. ' + this.settings.serverIP);
+			}
+		}
+	}
+
+	async download(){
+		const adapter = this.app.vault.adapter;
+		if (adapter instanceof FileSystemAdapter) {
+			const vaultPath = adapter.getBasePath();
+			const vaultPathArray = vaultPath.split('/')
+			const vaultName = vaultPathArray[vaultPathArray.length - 1] || "test_vault"
+			const tmpDir = path.join(os.tmpdir(), "obsidian-synchronizer-from-server");
+			const backupNames = await Toolchain.getAvailableBackupsFromServer(this.settings.serverIP, vaultName, this.settings.API_TOKEN)
+			
+			if (!fs.existsSync(tmpDir)) 
+				fs.mkdirSync(tmpDir, { recursive: true });
+
+		    new DownloadModal(this.app, backupNames, this.settings.serverIP, vaultName, vaultPath, tmpDir, this.settings.API_TOKEN).open();
+			new Notice('Archive have been successfully downloaded and unzip');
+		}
+	}
+
 	async onload() {
 		await this.loadSettings();
 		
 		// This creates an icon in the left ribbon.
 		this.addRibbonIcon('up-arrow-with-tail', 'Upload', async (evt: MouseEvent) => {
-			new Notice('Upload to server!' + ' Server IP: ' + this.settings.serverIP);
-			const adapter = this.app.vault.adapter;
-			
-			if (adapter instanceof FileSystemAdapter) {
-				const vaultPath = adapter.getBasePath();
-				const vaultPathArray = vaultPath.split('/')
-				const vaultName = vaultPathArray[vaultPathArray.length - 1]
-				const tmpDir = path.join(os.tmpdir(), "obsidian-synchronizer");
-				const zipPath = path.join(tmpDir, "vault-latest.zip");
-
-				if (!fs.existsSync(tmpDir)) 
-    				fs.mkdirSync(tmpDir, { recursive: true });
-				
-				await Toolchain.createZipArchive(vaultPath, zipPath)
-				const uploadResult = await Toolchain.uploadToServer(this.settings.serverIP, zipPath, vaultName as any, this.settings.API_TOKEN)
- 
-				if(uploadResult == 200) {
-					fs.rmdirSync(tmpDir, {recursive: true})
-					new Notice('Archive have been successfully uploaded to server. ' + this.settings.serverIP);
-				}
-			}
+			await this.upload()
 		});
 
 		this.addRibbonIcon('down-arrow-with-tail', 'Download', async (evt: MouseEvent) => {			
-			const adapter = this.app.vault.adapter;
-			if (adapter instanceof FileSystemAdapter) {
-				const vaultPath = adapter.getBasePath();
-				const vaultPathArray = vaultPath.split('/')
-				const vaultName = vaultPathArray[vaultPathArray.length - 1] || "test_vault"
-				const tmpDir = path.join(os.tmpdir(), "obsidian-synchronizer-from-server");
-				const backupNames = await Toolchain.getAvailableBackupsFromServer(this.settings.serverIP, vaultName, this.settings.API_TOKEN)
-				
-				if (!fs.existsSync(tmpDir)) 
-					fs.mkdirSync(tmpDir, { recursive: true });
+			await this.download
+		});
 
-			    new DownloadModal(this.app, backupNames, this.settings.serverIP, vaultName, vaultPath, tmpDir, this.settings.API_TOKEN).open();
+		this.addCommand({
+    		id: "upload",
+    		name: "Upload",
+    		icon: "refresh-cw",
+    		callback: async () => {
+    		    await this.upload
+    		}
+		});
 
-				new Notice('Archive have been successfully downloaded and unzip');
-			}
+		this.addCommand({
+    		id: "download",
+    		name: "Download",
+    		icon: "refresh-cw",
+    		callback: async () => {
+    		    await this.download
+    		}
 		});
 
 		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps. #TODO: додати статус репозиторія коли було завантажено, коли відвантажено
